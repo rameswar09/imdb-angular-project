9954dad1d23a01f756ef474a1a5f61742472579b
B RAMESWAR REDDY <rameswar.reddy@mountblue.io>
2019-04-21 15:28:14 +0530

    adding space

--- src/app/services/apiServices.ts
+++ src/app/services/apiServices.ts
@@ -12,6 +12,8 @@ export class apiServices {
     return data
   }
 
+
+  
   getTrendingPeoples = async () => {
     let data = await fetch('https://api.themoviedb.org/3/trending/person/day?api_key=3ebe7f44a343fc7c8c085ccfef4cc493&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZWJlN2Y0NGEzNDNmYzdjOGMwODVjY2ZlZjRjYzQ5MyIsInN1YiI6IjVjNWU3MmI0YzNhMzY4M2NjZjhkNDM1MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XYra3lypk70Rh69HEVdi76crIKkKJZE6KY350cEB3Ow', { method: 'get' })
     data = await data.json()
